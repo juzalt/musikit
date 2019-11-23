0ebdffc714f20537480326986b3cc98568f357d8
zxltrn <zaltron.julian@gmail.com>
2019-11-23 02:37:55 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import App from 'next/app'
+import React from 'react'
+import withReduxStore from '../lib/with-redux-store'
+import { Provider } from 'react-redux'
+
+class MyApp extends App {
+  render () {
+    const { Component, pageProps, reduxStore } = this.props
+    return (
+      <>
+      <Provider store={reduxStore}>
+        <Component {...pageProps} />
+      </Provider>
+      <style global jsx>{`
+              html,
+              body {
+                font-family: Roboto;
+                margin: 0;
+                padding: 0;
+                display: flex;
+                height: 100vh;
+                justify-content: center;
+                align-items: center;
+                background-color: #ecebeb;
+              }
+          `}</style>
+      </>
+    )
+  }
+}
+
+export default withReduxStore(MyApp)
---
+++ pages/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home(){
+  return(
+  <>
+    {/*<Sidebar>*/}
+    <div className="home__container">
+      Hola
+    </div>
+    {/*<Footer>*/} 
+  <style jsx>{`
+    .home__container {
+      background-color: hsla(0,0%,100%,.3);
+    }
+  `}</style>
+  </>
+  )
+}
+
+export default Home
