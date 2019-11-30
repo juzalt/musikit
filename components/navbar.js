195285425d23483758e28468613b5b8748bb162b
zxltrn <zaltron.julian@gmail.com>
2019-11-30 14:32:55 -0300

    kept going on navbar structure

--- components/navbar.js
+++ components/navbar.js
@@ -4,11 +4,23 @@ function Navbar(){
   return(
     <>
       <div className="container">
-        <div>
-          <div>Logo</div>
-          <div>Home</div>
-          <div>Search</div>
-        </div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="Spotify logo" className="navbar__large-logo"/>
+        <ul>
+          <li className="navbar-group">
+            <div className="navBar-item--with-icon-left">
+              <svg viewBox="0 0 512 512" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M 256.274 60.84 L 84.324 166.237 L 84.324 443.063 L 193.27 443.063 L 193.27 293.73 L 320.228 293.73 L 320.228 443.063 L 428.222 443.063 L 428.222 165.476 L 256.274 60.84 Z M 256.274 35.95 L 448.452 149.145 L 448.452 464.395 L 300 464.395 L 300 315.062 L 213.499 315.062 L 213.499 464.395 L 64.095 464.395 L 64.095 150.161 L 256.274 35.95 Z" fill="currentColor"></path></svg>
+              <div>Home</div>
+            </div>
+            <div className="navBar-item--with-icon-left">
+              <svg viewBox="0 0 512 512" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M349.714 347.937l93.714 109.969-16.254 13.969-93.969-109.969q-48.508 36.825-109.207 36.825-36.826 0-70.476-14.349t-57.905-38.603-38.603-57.905-14.349-70.476 14.349-70.476 38.603-57.905 57.905-38.603 70.476-14.349 70.476 14.349 57.905 38.603 38.603 57.905 14.349 70.476q0 37.841-14.73 71.619t-40.889 58.921zM224 377.397q43.428 0 80.254-21.461t58.286-58.286 21.461-80.254-21.461-80.254-58.286-58.285-80.254-21.46-80.254 21.46-58.285 58.285-21.46 80.254 21.46 80.254 58.285 58.286 80.254 21.461z" fill="currentColor" fill-rule="evenodd"></path></svg>
+              <div>Search</div>
+            </div>
+            <div className="navBar-item--with-icon-left">
+              <svg viewBox="0 0 512 512" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M291.301 81.778l166.349 373.587-19.301 8.635-166.349-373.587zM64 463.746v-384h21.334v384h-21.334zM192 463.746v-384h21.334v384h-21.334z" fill="currentColor"></path></svg>
+              <div>Your Library</div>
+            </div>
+          </li>
+        </ul>
         <div>
           Playlists
           <div>
@@ -17,18 +29,12 @@ function Navbar(){
           <p>
             Playlist1
           </p>
-          <p>
-            Playlist2
-          </p>
-          <p>
-            Playlist3
-          </p>
-          <p>
-            Playlist4
-          </p>
         </div>
       </div>
       <style jsx>{`
+        ul{
+          list-style: none;
+        }
         .container {
           display: flex;
           flex-direction: column;
@@ -37,7 +43,23 @@ function Navbar(){
           background-color: rgba(0,0,0,.8);
           overflow-y: auto;
           width: 230px;
-        } 
+          color: #b3b3b3;
+          font-size: 14px;
+          line-height: 20px;
+          letter-spacing: .015em;
+          font-weight: 600;
+          color: #b3b3b3;
+          text-transform: none;
+          font-family: spotify-circular,spotify-circular-cyrillic,spotify-circular-arabic,spotify-circular-hebrew,Helvetica Neue,Helvetica,Arial,Hiragino Kaku Gothic Pro,Meiryo,MS Gothic,sans-serif;
+        }
+        .navbar__large-logo{
+          max-inline-size: -webkit-fill-available;
+          margin: 1em;
+        }
+        .navBar-item--with-icon-left{
+          display: flex;
+          align-items: center;
+        }
       `}
       </style>
     </>
